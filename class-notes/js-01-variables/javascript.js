57df0b84d2d04afd1a12daf9aadfd4d33b8bbfb8
erkansevim <erkansevim.essgmail.com>
2024-05-03 10:27:19 +0200

    prompt

---
+++ class-notes/js-01-variables/javascript.js
@@ -0,0 +1,13 @@
+console.log("internal js");
+
+console.log("merhaba js");
+name1="ali";
+console.log(name1);
+alert("Error"); 
+
+//Alert kapatilmadan kod akmaya devam etmez//
+
+console.log("Alert closed");
+
+name2= prompt("adini gir");
+console.log(name2);
