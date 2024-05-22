ac67c2e87d6a1054f74f710464b48b2b04611aa6
erkansevim <erkansevim.essgmail.com>
2024-05-22 09:22:49 +0200

    exam

--- class-notes/recursive/recursive.js
+++ class-notes/recursive/recursive.js
@@ -9,23 +9,44 @@
 
 // //-------
 
-const toplamlar = (n) => {
-  if (n < 1) {
-    return n;
-  } else return toplamlar(n - 1) + n;
-};
+// const toplamlar = (n) => {
+//   if (n < 1) {
+//     return n;
+//   } else return toplamlar(n - 1) + n;
+// };
 
-console.log(toplamlar(499));
+// console.log(toplamlar(499));
 
 
-//---
+// //---
 
-const fibo=(n)=>{
+// const fibo=(n)=>{
 
-    if(n<=2){
-        return 1
-    }
-    else return fibo(n-1)+fibo(n-2)
+//     if(n<=2){
+//         return 1
+//     }
+//     else return fibo(n-1)+fibo(n-2)
+// }
+
+// console.log(fibo(6));
+
+//!decleration func
+
+islem (10,13)
+function islem(par1, par2, par3) {
+  console.log(par1);
 }
 
-console.log(fibo(6));
+
+//expression func
+const topla1= function (a,b) {
+  return a+b
+}
+
+console.log(topla1(3,5));
+
+//arrow func
+
+const topla2=(a,b)=> a+b;
+console.log(topla2(9,9));
+
